cd66a94cbd8df16dffce2b38b80a95c8c9a3a342
Travis Whitehead <whitehet@osuosl.org>
2018-10-18 13:26:07 -0700

    Initialize osl-git wrapper cookbook

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+osl-git CHANGELOG
+=================
+This file is used to list changes made in each version of the
+osl-git cookbook.
+
+0.1.0
+-----
+- Initial release of osl-git
+
---
+++ metadata.rb
@@ -0,0 +1,12 @@
+name             'osl-git'
+maintainer       'Oregon State University'
+maintainer_email 'chef@osuosl.org'
+license          'Apache-2.0'
+chef_version     '>= 12.18' if respond_to?(:chef_version)
+issues_url       'https://github.com/osuosl-cookbooks/osl-git/issues'
+source_url       'https://github.com/osuosl-cookbooks/osl-git'
+description      'Installs/Configures osl-git'
+long_description 'Installs/Configures osl-git'
+version          '0.1.0'
+
+supports         'centos', '~> 7.0'
