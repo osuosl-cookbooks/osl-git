ac5adbbfe258159effe1eec7d6285ff188e9f32b
Khuong Luu <6059347+khashf@users.noreply.github.com>
2019-04-25 00:54:57 -0700

    Fix test error for git-credential-databag

--- test/integration/git-credentials-databag/inspec/git_credentials_databag_spec.rb
+++ test/integration/git-credentials-databag/inspec/git_credentials_databag_spec.rb
@@ -4,8 +4,8 @@ end
 
 describe file '/root/.git-credentials' do
   it { should be_file }
-  its('owner') { should be 'root' }
-  its('mode') { should be '0600' }
+  its('owner') { should eq 'root' }
+  its('mode') { should cmp '0600' }
   [
     %r{https://name:token@example.123/hello/world.git},
     %r{bar:foo@example.xyz/foo/bar.git},
@@ -16,9 +16,12 @@ end
 
 describe file '/root/.gitconfig' do
   it { should be_file }
-  its('owner') { should be 'root' }
-  its('content') { should match %r{helper = store --file /root/\.git-credentials} }
-  its('content') { should match(/useHttpPath = true/) }
+  its('owner') { should eq 'root' }
+end
+
+describe ini('/root/.gitconfig') do
+  its('credential.helper') { should eq 'store --file /root/.git-credentials' }
+  its('credential.useHttpPath') { should eq 'true' }
 end
 
 describe file '/root/test/.git' do
@@ -27,8 +30,8 @@ end
 
 describe file '/home/foo/.git-credentials' do
   it { should be_file }
-  its('owner') { should be 'foo' }
-  its('mode') { should be '0600' }
+  its('owner') { should eq 'foo' }
+  its('mode') { should cmp '0600' }
   [
     %r{https://username:secret@example.456},
     /foobar:barfoo@example.abc/,
@@ -39,9 +42,12 @@ end
 
 describe file '/home/foo/.gitconfig' do
   it { should be_file }
-  its('owner') { should be 'foo' }
-  its('content') { should match %r{helper = store --file /home/foo/\.git-credentials} }
-  its('content') { should match(/useHttpPath = false/) }
+  its('owner') { should eq 'foo' }
+end
+
+describe ini('/home/foo/.gitconfig') do
+  its('credential.helper') { should eq 'store --file /home/foo/.git-credentials' }
+  its('credential.useHttpPath') { should eq 'false' }
 end
 
 describe file '/home/bar/.git-credentials' do
@@ -51,6 +57,9 @@ end
 describe file '/home/bar/.gitconfig' do
   it { should be_file }
   it { should be_owned_by 'bar' }
-  its('content') { should_not match(/helper/) }
-  its('content') { should_not match(/useHttpPath/) }
+end
+
+describe ini('/home/bar/.gitconfig') do
+  its('credential.helper') { should eq nil }
+  its('credential.useHttpPath') { should eq nil }
 end
