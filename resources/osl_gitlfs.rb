df5b2353f9df5f3b2a30b42c4d50f8b219881579
David Headrick <daviddjh@osuosl.org>
2020-05-29 10:09:51 -0700

    updated proerties and resources

--- resources/osl_gitlfs.rb
+++ resources/osl_gitlfs.rb
@@ -1,23 +1,24 @@
 resource_name :osl_gitlfs
 
-default_action :checkout
+default_action :sync
 
 provides :osl_gitlfs
-property :repository, String
+property :repository, String, required: true
+property :directory, String, required: true
 
-action :checkout do
+action :sync do
 
   run_context.include_recipe 'base::gitlfs'
+  run_context.include_recipe 'git'
 
-  bash do
-    cwd new_resource.name
-    code "git clone #{new_resource.repository}" 
+  git new_resource.directory do
+    repository new_resource.repository
     environment ({"GIT_LFS_SKIP_SMUDGE" => "1"})
   end
 
-  bash do
-    cwd new_resource.name
-    code "git lfs checkout"
+  bash 'lfs checkout repository' do
+    cwd "#{new_resource.directory}/#{new_resource.name}"
+    code "git lfs pull"
   end
 
 end
