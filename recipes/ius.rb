4c1b3d92697f761b79fe31fa60c76c6b55a03636
Robert Detjens <detjensrobert@osuosl.org>
2020-08-03 14:26:05 -0700

    Add recipe to install git from IUS repo

---
+++ recipes/ius.rb
@@ -0,0 +1,24 @@
+#
+# Cookbook:: osl-git
+# Recipe:: ius
+#
+# Copyright:: 2020, Oregon State University
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+include_recipe 'yum-ius'
+
+git_client 'ius git224' do
+  package_name 'git224'
+  action :install
+  only_if { platform_family?('rhel') && node['platform_version'].to_i == 7 } # IUS not available on C8
+end
