f3bb42815e72fcd59d46d9ebb136b62bc8f8136a
Lance Albertson <lance@osuosl.org>
2021-06-11 13:07:49 -0700

    Raise error on CentOS 8 if using ius recipe
    The IUS repository does not support CentOS 8 and higher so we should raise an
    error if that is included.
    
    In addition, add CentOS 8 to ChefSpec and update tests.
    
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- recipes/ius.rb
+++ recipes/ius.rb
@@ -15,9 +15,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+raise 'IUS is only supported on CentOS 7' if platform_family?('rhel') && node['platform_version'].to_i >= 8
+
 include_recipe 'yum-ius'
 
 git_client "ius #{node['osl-git']['ius_git_package']}" do
   package_name node['osl-git']['ius_git_package']
-  only_if { platform_family?('rhel') && node['platform_version'].to_i == 7 } # IUS not available on C8
 end
