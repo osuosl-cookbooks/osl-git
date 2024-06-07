a7a44aca5f57686b4256d6b8ef2dadfff097bd9a
Sharon Bousso <sharon@amber.workstation.cass.oregonstate.edu>
2024-06-07 16:33:10 -0700

    Added alma 9 to spec tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,11 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
 
+ALMA_9 = {
+  platform: 'almalinux',
+  version: '9',
+}.freeze
+
 ALMA_8 = {
   platform: 'almalinux',
   version: '8',
@@ -17,6 +22,7 @@ DEBIAN_12 = {
 }.freeze
 
 ALL_PLATFORMS = [
+  ALMA_9,
   ALMA_8,
   CENTOS_7,
   DEBIAN_12,
