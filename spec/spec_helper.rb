27315d12816ba5e6c8ca5e8d1aa4160fdafcc82b
Lance Albertson <lance@osuosl.org>
2024-07-31 14:28:30 -0700

    Add support for Ubuntu
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,10 +16,21 @@ DEBIAN_12 = {
   version: '12',
 }.freeze
 
+UBUNTU_2404 = {
+  platform: 'ubuntu',
+  version: '24.04',
+}.freeze
+
+ALL_DEBIAN = [
+  DEBIAN_12,
+  UBUNTU_2404,
+].freeze
+
 ALL_PLATFORMS = [
   ALMA_9,
   ALMA_8,
   DEBIAN_12,
+  UBUNTU_2404,
 ].freeze
 
 RSpec.configure do |config|
